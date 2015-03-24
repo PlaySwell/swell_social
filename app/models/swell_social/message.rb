d4f70d3eb712356ef22f56b436d4633274f0e67e
Michael <meister@spacekace.com>
2015-03-24 08:55:25 -0700

    add status to messages

--- app/models/swell_social/message.rb
+++ app/models/swell_social/message.rb
@@ -2,6 +2,8 @@ module SwellSocial
 	class Message < ActiveRecord::Base
 		self.table_name = 'messages'
 
+		enum status: { 'unread' => 1, 'read' => 2, 'archived' => 3, 'trash' => 4 }
+
 		
 	end
 end
