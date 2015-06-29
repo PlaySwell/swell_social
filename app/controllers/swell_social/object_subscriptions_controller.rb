88e15b52b58ed2f7b10629cb1f0c4a6d204867dd
danielbogart <daniel.e.bogart>
2015-06-28 17:54:30 -0700

    added async functionality for follow/unfollow buttons

--- app/controllers/swell_social/object_subscriptions_controller.rb
+++ app/controllers/swell_social/object_subscriptions_controller.rb
@@ -6,26 +6,36 @@ module SwellSocial
 
 		def create
 			@sub = ObjectSubscription.where( user_id: current_user.id, parent_obj_type: params[:obj_type], parent_obj_id: params[:obj_id] ).first_or_initialize
-			if @sub.active!
+
+		    respond_to do |format|
+		      if @sub.active!
 				@sub.parent_obj.increment!( :cached_subscribe_count ) if @sub.parent_obj.respond_to?( :cached_subscribe_count )
 				record_user_event( 'obj_subscribe', user: current_user, on: @sub.parent_obj, content: "subscribed to the #{@sub.parent_obj.class.name.downcase} <a href='#{@sub.parent_obj.url}'>#{@sub.parent_obj.to_s}</a>!" ) if defined?( SwellPlay )
-				set_flash "Subscribed"
-			else
-				set_flash "Could not subscribe", :error, @sub
-			end
-			redirect_to :back
+		        format.html { redirect_to(:back, set_flash: 'Subscribed') }
+		        format.js {}
+		      else
+		        format.html { redirect_to(:back, set_flash: 'Error') }
+		        format.js {}
+		      end
+		    end
+
 		end
 
 
 		def destroy
 			@sub = ObjectSubscription.active.where( user_id: current_user.id ).find_by( id: params[:id] )
-			if @sub.deleted!
-				@sub.parent_obj.decrement!( :cached_subscribe_count ) if @sub.parent_obj.respond_to?( :cached_subscribe_count )
-				set_flash "Unsubscribed"
-			else
-				set_flash "Could not unsubscribe", :error, @sub
+			
+			respond_to do |format|
+				if @sub.deleted!
+					@sub.parent_obj.decrement!( :cached_subscribe_count ) if @sub.parent_obj.respond_to?( :cached_subscribe_count )
+			        format.html { redirect_to(:back, set_flash: 'Unsubscribed') }
+			        format.js {}
+				else
+			        format.html { redirect_to(:back, set_flash: 'Could not unsubscribe') }
+			        format.js {}
+				end
 			end
-			redirect_to :back
+
 		end
 
 	end
