2fc8520a98daa3350b7d338961b0ae764ff8455c
Gk <gk@gkparishphilp.com>
2015-09-15 09:49:42 -0700

    - ?

--- app/controllers/swell_social/object_subscriptions_controller.rb
+++ app/controllers/swell_social/object_subscriptions_controller.rb
@@ -11,7 +11,7 @@ module SwellSocial
 			respond_to do |format|
 			  if @sub.active!
 				@sub.parent_obj.increment!( :cached_subscribe_count ) if @sub.parent_obj.respond_to?( :cached_subscribe_count )
-				record_user_event( 'obj_subscribe', user: current_user, on: @sub.parent_obj, content: "subscribed to the #{@sub.parent_obj.class.name.downcase} <a href='#{@sub.parent_obj.url}'>#{@sub.parent_obj.to_s}</a>!" ) if defined?( SwellPlay )
+				record_user_event( "#{@sub.parent_obj.class.name.demodulize.underscore}_subscribe", user: current_user, on: @sub.parent_obj, content: "subscribed to the #{@sub.parent_obj.class.name.downcase} <a href='#{@sub.parent_obj.url}'>#{@sub.parent_obj.to_s}</a>!" )
 				format.html { redirect_to(:back, set_flash: 'Subscribed') }
 				format.js {}
 			  else
@@ -30,6 +30,7 @@ module SwellSocial
 			respond_to do |format|
 				if @sub.deleted!
 					@sub.parent_obj.decrement!( :cached_subscribe_count ) if @sub.parent_obj.respond_to?( :cached_subscribe_count )
+					record_user_event( "#{@sub.parent_obj.class.name.demodulize.underscore}_unsubscribe", user: current_user, on: @sub.parent_obj, content: "unsubscribed from the #{@sub.parent_obj.class.name.downcase} <a href='#{@sub.parent_obj.url}'>#{@sub.parent_obj.to_s}</a>!" )
 					format.html { redirect_to(:back, set_flash: 'Unsubscribed') }
 					format.js {}
 				else
