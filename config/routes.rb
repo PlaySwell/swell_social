640e5fd5601c01d05b0d271fe405084636fda9f3
Michael <meister@spacekace.com>
2016-02-10 08:54:44 -0800

    adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SwellSocial::Engine.routes.draw do
 	
-	post '/comment_on/:type/:id' => 'user_posts#create', as: 'comment_on'
+	match '/comment_on/:type/:id' => 'user_posts#create', as: 'comment_on', via: [:get, :post]
 
 	resources :discussions do 
 		resources :topics, controller: :discussion_topics
@@ -16,6 +16,6 @@ SwellSocial::Engine.routes.draw do
 		get :admin, on: :collection
 	end
 
-	resources :votes, only: [:create, :destroy, :update]
+	resources :votes, only: [:create, :destroy, :update, :index]
 
 end
