4cefd5d4314dbd3d0cb52fad70be6e59d774109b
Michael <meister@spacekace.com>
2016-03-24 08:50:25 -0700

    a few fixes for new comments

--- app/controllers/swell_social/comments_controller.rb
+++ app/controllers/swell_social/comments_controller.rb
@@ -15,7 +15,7 @@ module SwellSocial
 			else
 				@parent_obj = params[:parent_obj_type].constantize.find(params[:parent_obj_id])
 
-				@comments = @parent_obj.try(params[:comment_attribute] || :comments, order: :desc )
+				@comments = @parent_obj.try(params[:comment_attribute] || :comments, order: @direction )
 				@comments ||= @parent_obj.try(params[:comment_attribute] || :comments)
 				@comments ||= UserPost.where( parent_obj_id: @parent_obj.id, parent_obj_type: @parent_obj.class.name )
 				@comments = @comments.where( reply_to_id: nil )
