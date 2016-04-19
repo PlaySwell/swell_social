cd1ea08c4cfa1b60891ab9168e167ed38ff29bb0
Michael <meister@spacekace.com>
2016-04-19 16:09:06 -0700

    functons for notifications

--- app/models/swell_social/notification.rb
+++ app/models/swell_social/notification.rb
@@ -12,12 +12,56 @@ module SwellSocial
 		belongs_to		:parent_obj, polymorphic: true
 		belongs_to		:activity_obj, polymorphic: true
 
+		has_many :actors, through: :children
+
 		def self.active
 			where( status: [1,2,3] )
 		end
 
 		def self.not_read
 			where( status: [1,2] )
+		end
+
+		def actor_list
+			if children_count > 0
+				self.actors.reorder( 'notifications.created_at ASC' )
+			else
+				SwellMedia.registered_user_class.constantize.where( id: self.actor_id )
+			end
+		end
+
+		def actor_str( args = {} )
+
+			args[:max_actors] ||= 3
+
+			total_actors = self.actor_list.count
+
+			actor_shortlist = self.actor_list.limit(args[:max_actors])
+
+			actor_shortlist_join_count = [(actor_shortlist.count-2),0].max
+
+			message_actors = actor_shortlist[0..actor_shortlist_join_count].collect{|user| "<a href='#{user.url}'>#{user}</a>" }.join(', ')
+
+			if total_actors > actor_shortlist.count
+				message_actors = "#{message_actors} and #{total_actors - actor_shortlist.count} others"
+			elsif actor_shortlist.count > 1
+				message_actors = "#{message_actors} and <a href='#{actor_shortlist.last.url}'>#{actor_shortlist.last}</a>"
+			end
+
+			message_actors
+
+		end
+
+		def sanitized_actor_message
+			ActionView::Base.full_sanitizer.sanitize( self.actor_str )
+		end
+
+		def sanitized_title
+			ActionView::Base.full_sanitizer.sanitize( self.title )
+		end
+
+		def to_s
+			"#{self.actor_str} #{self.title}"
 		end
 
 		def active?
